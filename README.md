d7d8c9a6cf282ea09af01caae093bd13a1f4f828
jeremy-rifkin <rifkin.jer@gmail.com>
2019-09-07 00:24:14 -0400

    Rewrote aggregate script.  My old code was really bad.

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ The objective of this project is to combine the many English word lists availabl
 - [Wordlist](#wordlist)
 	- [Contents](#contents)
 - [Resources](#resources)
+- [Running the aggregator yourself](#running-the-aggregator-yourself)
 - [Licence](#licence)
 
 # Resources
@@ -23,6 +24,16 @@ This master word list is compiled by a Python script using the following resourc
 | c.txt     | 109583        | http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt |
 | d.txt     | 58110         | http://www.mieliestronk.com/corncob_lowercase.txt                        |
 | e.txt     | 274926        | *unknown...* |
+
+# Running the aggregator yourself
+
+```bash
+git clone https://github.com/jeremy-rifkin/Wordlist.git
+cd Wordlist
+python aggregate.py res/* > master.txt
+```
+
+Want to add your own word list? Just add it to `res/`.
 
 # Licence
 
--- aggregate.py
+++ aggregate.py
@@ -1,39 +1,38 @@
-'''''''''''''''''''''''''''''''''''''''
-''  Copyright (c) FelisPhasma 2015   ''
-'''''''''''''''''''''''''''''''''''''''
-import time
+# Word list aggregate script
+# Copyright (c) Jeremy Rifkin 2015 - 2019
+
 import os
-current_milli_time = lambda: int(round(time.time() * 1000))
-start = current_milli_time()
-sourcePath = "res/"
-sourceFiles = ['a.txt', 'c.txt', 'd.txt', 'e.txt']
-#'b.txt',
-# B excluded because of ascii control characters
-def end():
-	execTime = current_milli_time() - start
-	print "Execution time: ", str(current_milli_time() - start), "ms"
-	print "Execution time: ", str((current_milli_time() - start) / 1000), "sec"
-	print "Execution time: ", str((current_milli_time() - start) / (60 * 1000)), "min"
-	print "Execution time: ", str(((current_milli_time() - start) / (60 * 1000)) / 60), "hr"
-def init():
-	print("Init")
-	tList = set()
-	for fname in sourceFiles:
-		with open(sourcePath + fname, "r") as infile:
-			for line in infile:
-				tList.add(line.lower().capitalize())
-	print("Words compiled")
-	dList = set()
-	for i in tList:
-		if i not in dList:
-			dList.add(i)
-	tList = None
-	print("Duplicates removed")
-	output = sorted(dList)
-	dList = None
-	outputFile = open("master.txt", "w+")
-	for line in output:
-		outputFile.write(line)
-	end()
-	print("Done")
-init()
+import sys
+import time
+
+def main():
+	# Check that files were given
+	if len(sys.argv) == 1:
+		sys.stderr.write("Error: no files provided.\n")
+		sys.exit(1)
+	# Check that all files exist
+	files = sys.argv[1:]
+	for fpath in files:
+		if not os.path.isfile(fpath):
+			sys.stderr.write("Error: file \"{}\" does not exist.\n".format(fpath))
+	
+	sys.stderr.write("Starting aggregator.\n")
+	start = time.time()
+	
+	words = {} # Would initialize this dict to be large if python would let me to save the effort of continuous re-hashing, but oh well. This should run fast anyway.
+
+	for fpath in files:
+		with open(fpath, "r") as f:
+			for line in f:
+				if line.strip() != "":
+					words[line.strip().capitalize()] = 0
+	
+	words = sorted(words.keys())
+	for word in words:
+		print(word)
+
+	sys.stderr.write("Done.\n")
+	t = time.time() - start
+	sys.stderr.write("Excecution time: {:.02f} second{}.\n".format(t, "" if t == 1 else "s"))
+
+main()
